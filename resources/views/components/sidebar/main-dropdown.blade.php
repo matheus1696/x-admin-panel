8069787a9f1af01dbe1071e0bef0e73c9e1703aa
Matheus Mota <matheus.mat1996@gmail.com>
2026-01-16 23:26:33 -0300

    X-AdminPanel v1.0.6 - correção de bug barra de navegacao transparent

--- resources/views/components/sidebar/main-dropdown.blade.php
+++ resources/views/components/sidebar/main-dropdown.blade.php
@@ -18,7 +18,7 @@
             <i class="{{ $icon }} w-5 text-center text-sm {{ $active ? 'text-white' : 'text-green-600' }}"></i>
 
             <span
-                :class="sidebarExpanded ? 'md:opacity-100' : 'md:hidden md:opacity-0'"
+                :class="sidebarExpanded ? 'md:opacity-100' : 'md:opacity-0'"
                 class="transition-all duration-200 whitespace-nowrap"
             >
                 {{ $title }}
